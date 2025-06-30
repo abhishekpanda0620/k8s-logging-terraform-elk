5be88afcf7024c0d427df80b0372431c6c15f438
Abhishek Panda <abhishek.panda0620@gmail.com>
2025-06-30 13:02:57 +0530

    Refactor Terraform configuration for improved readability

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = var.backend_s3_bucket
-    key    = var.backend_s3_key
-    region = var.aws_region
+    bucket       = var.backend_s3_bucket
+    key          = var.backend_s3_key
+    region       = var.aws_region
     use_lockfile = true
 
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,19 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-     kubernetes = {
-      source = "hashicorp/kubernetes"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
       version = "2.37.1"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
   # Optionally specify profile if using named AWS CLI profiles
   # profile = "default"
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
