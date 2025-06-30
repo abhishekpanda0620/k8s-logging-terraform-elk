ee8c830c9f4da736cf0bd2f2e87cd46055e16f0f
Abhishek Panda <abhishek.panda0620@gmail.com>
2025-06-30 14:47:24 +0530

    Update .gitignore and simplify S3 backend configuration in Terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket       = var.backend_s3_bucket
-    key          = var.backend_s3_key
-    region       = var.aws_region
-    use_lockfile = true
-
-  }
+  backend "s3" {}
 }
